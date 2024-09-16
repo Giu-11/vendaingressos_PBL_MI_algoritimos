d82acb64ecb4a60bd1394053d975120df8e9e1f1
Giu <120753646+Giu-11@users.noreply.github.com>
2024-09-16 13:03:26 -0300

    📚docs: correção de erros nos comentarios

--- src/vendaingressos/Controller.java
+++ src/vendaingressos/Controller.java
@@ -1,7 +1,7 @@
 /*******************************************************************************************
 Autor: Giulia Aguiar Loula
 Componente Curricular: EXA863 - MI - PROGRAMAÇÃO
-Concluído em: 07/09/22024
+Concluído em: 07/09/2024
 Declaro que este código foi elaborado por mim de forma individual e não contêm nenhum
 trecho de código de outro colega ou de outro autor, tais como provindos de livros e
 apostilas, e páginas ou documentos eletrônicos da Internet. Qualquer trecho de código
